60591b69db72199864c70ad5d8422b36d1284f63
threepebbles <threepebbles@naver.com>
2024-07-19 10:06:02 +0900

    chore: initial setting

---
+++ src/main/java/com/example/java21_practice/Java21PracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.java21_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java21PracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java21PracticeApplication.class, args);
+	}
+
+}
