f10fd5430748b81a7ecf16a24e205a052209df51
threepebbles <threepebbles@naver.com>
2024-07-21 01:03:20 +0900

    feat(util): 랜덤 유틸 기능 추가

--- src/main/java/com/example/practiceShop/util/RandomUtil.java
+++ src/main/java/com/example/practiceShop/util/RandomUtil.java
@@ -1,2 +1,7 @@
-package com.example.practiceShop.util;public class RandomUtil {
+package com.example.practiceShop.util;
+
+public class RandomUtil {
+    public static Long generateRandomNumber(Long mi, Long mx) {
+        return (long) ((Math.random() * (mx - mi)) + mi);
+    }
 }
