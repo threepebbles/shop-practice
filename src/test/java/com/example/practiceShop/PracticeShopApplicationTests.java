ff0cbe369fcac7a27b51dc257f029e5682b8c4a7
threepebbles <threepebbles@naver.com>
2025-07-18 12:08:37 +0900

    chore: dev 브랜치 ci 테스트를 위한 의미없는 커밋

--- src/test/java/com/example/practiceShop/PracticeShopApplicationTests.java
+++ src/test/java/com/example/practiceShop/PracticeShopApplicationTests.java
@@ -1,16 +1,9 @@
 package com.example.practiceShop;
 
-import com.example.practiceShop.test.HelloEntity;
-import com.example.practiceShop.test.HelloService;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Profile;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class PracticeShopApplicationTests {
-
 }
